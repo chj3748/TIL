3c10f4e97ca7e7a840e4014c43a5adfeba431fcb
hojin <redsmile123@naver.com>
2021-05-03 19:39:21 +0900

    2458 시간복잡도 개선 및 다른 풀이

--- Algorithm/baekjoon/bj_2458.py
+++ Algorithm/baekjoon/bj_2458.py
@@ -5,7 +5,7 @@ import sys
 # from math import factorial
 # from collections import deque
 
-sys.setrecursionlimit(int(1e9))
+# sys.setrecursionlimit(int(1e9))
 inp = sys.stdin.readline
 
 n, m = map(int, inp().split())
@@ -24,15 +24,13 @@ answer = 0
 # 기준 학생보다 키가 작은 학생을 전부 찾아내서 반환
 def top_down_check(student):
     smaller = 0
-    stack = []
-    checked = []
-    stack.append(student)
+    stack = [student]
     while stack:
         someone = stack.pop()
         for next_student in top_down[someone]:
-            if next_student not in checked:
+            if not checked[next_student]:
                 stack.append(next_student)
-                checked.append(next_student)
+                checked[next_student] = 1
                 smaller += 1
     return smaller
 
@@ -40,21 +38,58 @@ def top_down_check(student):
 # 기준학생보다 키가 큰 학생을 전부 찾아서 반환
 def bottom_up_check(student):
     taller = 0
-    stack = []
-    checked = []
-    stack.append(student)
+    stack = [student]
     while stack:
         someone = stack.pop()
         for next_student in bottom_up[someone]:
-            if next_student not in checked:
+            if not checked[next_student]:
                 stack.append(next_student)
-                checked.append(next_student)
+                checked[next_student] = 1
                 taller += 1
     return taller
 
 
 for i in range(1, n + 1):
-    if top_down_check(i) + bottom_up_check(i) == n - 1:
+    checked = [0]*(n+1)
+    td_cnt = top_down_check(i)
+    bu_cnt = bottom_up_check(i)
+    if td_cnt + bu_cnt == n - 1:
         answer += 1
 
 print(answer)
+
+
+# 다른 풀이
+## 훨씬 빠름
+### 각 학생에서 키가 크던 작던 상관없이 깊이 탐색을 하면서
+### 찾은 학생마다 +1을 해줌
+### 그 결과 edge_cnt 테이블에는 각 학생이 몇번 호출 됐는지가 체크 됨
+### 즉 본인 제외 모든 학생에게 호출된 학생은 모든 학생과 연결된 키순서가 있다는 것
+
+# def dfs(start, cur):
+#     if start != cur:
+#         edge_cnt[start] += 1
+#         edge_cnt[cur] += 1
+#     visited[cur] = True
+#     for w in graph[cur]:
+#         if not visited[w]:
+#             dfs(start, w)
+#
+#
+# N, M = list(map(int, input().split()))
+# graph = [[] for _ in range(N+1)]
+# ans = 0
+# for _ in range(M):
+#     a, b = list(map(int, input().split()))
+#     graph[a].append(b)
+#
+# edge_cnt = [0] * (N+1)
+# for node in range(1, N+1):
+#     visited = [False] * (N+1)
+#     dfs(node, node)
+#
+# for node in range(1, N+1):
+#     if edge_cnt[node] == N-1:
+#         ans += 1
+#
+# print(ans)
