5180b97db8d2619c63c64fe20d352e6340a2afd2
hojin <redsmile123@naver.com>
2021-05-05 20:23:58 +0900

    11660 다시 풀기

--- Algorithm/baekjoon/bj_11660.py
+++ Algorithm/baekjoon/bj_11660.py
@@ -1,8 +1,8 @@
-# dp | bj 11660 구간 합 구하기 5
+# dp 누적합 | bj 11660 구간 합 구하기 5
 import sys
 input = sys.stdin.readline
 
-# 1
+# 1 시간초과
 # N, M = map(int, input().split())
 # maps = [ list(map(int,input().split())) for _ in range(N) ]
 
@@ -32,4 +32,5 @@ for i in range(1, N):
         dp[i][j] = arr[i][j] + dp[i-1][j] + dp[i][j-1] -dp[i-1][j-1]
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    print(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1])#어려웠던 부분...
+    # 가장 큰 범위에서 구해야하는 바깥 범위 두곳을 빼주고 중복되어서 제거된 곳은 한번 더해 준다
+    print(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1])
