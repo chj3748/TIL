d7cd662fa8343c7ef6dbe7bd2c1e2115f22e9841
hojin <redsmile123@naver.com>
2021-11-01 11:37:37 +0900

    풀이 추가

--- Algorithm/programmers/pg_큰 수 만들기.py
+++ Algorithm/programmers/pg_큰 수 만들기.py
@@ -7,6 +7,31 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
+# 첫번째 풀이
+def solution(number, k):
+    from collections import deque
+    stack = []
+    number = deque(list(map(int, number)))
+    while number and k > 0:
+        num = number.popleft()
+        if stack:
+            while k > 0 and stack and stack[-1] < num:
+                stack.pop()
+                k -= 1
+        if k > 0 and number and num < number[0]:
+            k -= 1
+            continue
+        stack.append(num)
+
+    for num in number:
+        stack.append(num)
+
+    for _ in range(k):
+        stack.pop()
+    return ''.join(map(str, stack))
+
+
+# 두번째 풀이(개선안)
 def solution(number, k):
     stack = []
     for num in number:
