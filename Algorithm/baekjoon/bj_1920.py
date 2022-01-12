c989c9d7c499ba1330ecfe39600d2e810cdc7958
hojin <redsmile123@naver.com>
2022-01-12 14:44:20 +0900

    풀이 추가 | 이분탐색 해시 | bj 1920 수 찾기

--- Algorithm/baekjoon/bj_1920.py
+++ Algorithm/baekjoon/bj_1920.py
@@ -1,29 +1,42 @@
-# 이분탐색 | bj 1920 수 찾기
-import sys
-import bisect
+# 이분탐색 해시 | bj 1920 수 찾기
+# github.com/chj3748
 
-inp = sys.stdin.readline
 
-N = int(inp())
-arr = list(map(int, inp().split()))
-M = int(inp())
-
-# O(logn)
-arr.sort()
-find_number = list(map(int, inp().split()))
-for f_number in find_number:
-    idx = bisect.bisect_left(arr, f_number)
-    # print(idx, f_number, arr)
-    if not (arr[0] <= f_number <= arr[-1]):
-        print(0)
-    elif arr[idx] == f_number:
-        print(1)
-    else:
-        print(0)
-
-# # O(n)
+# 풀이 1
+# import sys
+# import bisect
+#
+# inp = sys.stdin.readline
+#
+# N = int(inp())
+# arr = list(map(int, inp().split()))
+# M = int(inp())
+#
+# arr.sort()
+# find_number = list(map(int, inp().split()))
 # for f_number in find_number:
-#     if f_number in arr:
+#     idx = bisect.bisect_left(arr, f_number)
+#     if not (arr[0] <= f_number <= arr[-1]):
+#         print(0)
+#     elif arr[idx] == f_number:
 #         print(1)
 #     else:
 #         print(0)
+
+# 새로운 풀이
+import sys
+from collections import defaultdict
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+N = int(input())
+numbers = defaultdict(int)
+for num in list(map(int, input().split())):
+    numbers[num] = 1
+
+M = int(input())
+for find_num in list(map(int, input().split())):
+    print(1 if numbers[find_num] else 0)
